6d5e617e170a0f3f24ac41ce7f3721974a69d7f7
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2017-07-24 23:40:20 +0300

    Add console project

---
+++ UsersManagement/UM.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UM.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
